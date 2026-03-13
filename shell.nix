7fc1c569208ad2556eba0c41f322b499c8733622
3top1a <filip@rusz.space>
2026-03-13 10:48:39 +0100

    Fixed shell.nix

--- shell.nix
+++ shell.nix
@@ -1,22 +1,44 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  # Create an environment with all necessary libraries
+  libEnv = pkgs.buildEnv {
+    name = "invenio-libs";
+    paths = with pkgs; [
+      cairo
+      pango
+      gdk-pixbuf
+      glib
+      libxml2
+      libxslt
+      zlib
+    ];
+  };
+in
+
 pkgs.mkShell {
   buildInputs = with pkgs; [
     # Python
     uv
 
-    # library dependencies
-    libxml2
-    libxslt
-    zlib
-	cairosvg # https://cairosvg.org/
-    
     # Build tools
     pkg-config
     gcc
 
-	nodejs_20
-	docker
-	imagemagick
+    nodejs_20
+    docker
+    imagemagick
   ];
+
+  nativeBuildInputs = [ libEnv ];
+
+  shellHook = ''
+    export UV_SYSTEM_PYTHON=0
+    unset PYTHONPATH
+    
+    export LD_LIBRARY_PATH="${libEnv}/lib:$LD_LIBRARY_PATH"
+    export PKG_CONFIG_PATH="${libEnv}/lib/pkgconfig:$PKG_CONFIG_PATH"
+    export DYLD_LIBRARY_PATH="${libEnv}/lib:$DYLD_LIBRARY_PATH"
+  '';
 }
+
